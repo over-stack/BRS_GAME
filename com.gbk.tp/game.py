020ac0db6c48b69fd20e09260cfc23e564914bb7
michael <gibiktus@mail.ru>
2019-12-26 21:48:52 +0300

    session

--- com.gbk.tp/game.py
+++ com.gbk.tp/game.py
@@ -4,17 +4,46 @@ import time
 
 
 class Subject:
-    def __init__(self, position):
+    def __init__(self, position, text):
         self.position = position
+        self.text = text
         self.percent = 100.
+        self.request = False
+        self.active = False
+        self.inuse = False
+        self.start_time = time.monotonic()
+        self.duration = random.uniform(0, 1) * 10
 
     def get_colour(self):
-        return ((100-self.percent) * 2.55, self.percent * 2.55, 0)
+        return (100-self.percent) * 2.55, self.percent * 2.55, 0
 
-    def decrease(self, speed, time_):
-        self.percent -= speed * time_
+    def decrease(self, speed):
+        self.percent -= speed
         if self.percent < 0:
             self.percent = 0
+
+    def activate(self, speed):
+        self.inuse = True
+        if not self.active:
+            if not self.request:
+                self.start_time = time.monotonic()
+                self.duration = random.uniform(0, 1) / speed
+                self.request = True
+
+    def update(self, speed):
+        if self.request:
+            current_time = time.monotonic()
+            if current_time > self.start_time + self.duration:
+                self.active = True
+                self.request = False
+
+        if self.active:
+            self.decrease(speed)
+
+    def touch(self):
+        self.active = False
+        self.percent = 100.
+        self.inuse = False
 
 
 class Game:
@@ -33,19 +62,22 @@ class Game:
 
         self.fonts = {'score': pygame.font.SysFont('Comic Sans MS', 30),
                       'percents': pygame.font.SysFont('Times New Roman', 18),
-                      'gameover': pygame.font.SysFont('Comic Sans MS', 30)}
+                      'gameover': pygame.font.SysFont('Comic Sans MS', 30),
+                      'session': pygame.font.SysFont('Comic Sans MS', 30)}
         self.fonts['percents'].set_bold(True)
         self.fonts['gameover'].set_bold(True)
+        self.fonts['session'].set_bold(True)
 
         self.subjects = list()
-        self.subjects.append(Subject((44, 35)))
-        self.subjects.append(Subject((44, 120)))
-        self.subjects.append(Subject((44, 205)))
-        self.subjects.append(Subject((44, 275)))
-        self.subjects.append(Subject((44, 345)))
-        self.subjects.append(Subject((44, 420)))
-        self.subjects.append(Subject((44, 495)))
-        self.subjects.append(Subject((44, 565)))
+        self.subjects.append(Subject((44, 35), 'Дифференциальные уравнения'))
+        self.subjects.append(Subject((44, 120), 'Математический анализ'))
+        self.subjects.append(Subject((44, 205), 'Языки программирования'))
+        self.subjects.append(Subject((44, 275), 'Безопасность жизнедеятельности'))
+        self.subjects.append(Subject((44, 345), 'Иностранный язык'))
+        self.subjects.append(Subject((44, 420), 'Пакеты компьютерной алгебры'))
+        self.subjects.append(Subject((44, 495), 'Практикум по ЭВМ'))
+        self.subjects.append(Subject((44, 565), 'Философия'))
+        self.subjects.append(Subject((44, 625), 'Экономика и право'))
 
         self.background = pygame.image.load('background.png')
         self.final_screen = pygame.image.load('gameover.jpg')
@@ -56,10 +88,16 @@ class Game:
         self.start_time = time.monotonic()
         self.duration = random.uniform(0, 1) * 10
         self.speed = 2.
-        self.delta = 0.3
+        self.delta = 0.5
         self.gameover = False
 
         self.score = 0
+        self.progress = 0
+        self.progress_speed = 0.4
+        self.session = False
+        self.session_start = time.monotonic()
+        self.session_duration = random.uniform(0, 1) * 100
+        self.max_activities = 1
 
     def run(self):
         clock = pygame.time.Clock()
@@ -73,14 +111,13 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
 
-                if self.action:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        pos = pygame.mouse.get_pos()
-                        if (pos[0] - self.subjects[self.choice].position[0]) ** 2 + \
-                                        (pos[1] - self.subjects[self.choice].position[1]) ** 2 <= 25 ** 2:
-                            self.action = False
-                            self.subjects[self.choice].percent = 100.
-                            self.score += 1
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    for subject in self.subjects:
+                        if subject.active:
+                            if (pos[0] - subject.position[0]) ** 2 + (pos[1] - subject.position[1]) ** 2 <= 625:
+                                subject.touch()
+                                self.score += 1
 
             if not self.gameover:
                 self.update(time)
@@ -89,23 +126,34 @@ class Game:
         pygame.quit()
 
     def update(self, time_):
-        if not self.action:
-            if not self.request:
-                self.choice = int(random.uniform(0, 1) * len(self.subjects))
-                self.start_time = time.monotonic()
-                self.duration = random.uniform(0, 1) * 10 / self.speed
-                self.request = True
-                self.speed += self.delta
-            else:
-                current_time = time.monotonic()
-                if (current_time > self.start_time + self.duration):
-                    self.action = True
-                    self.request = False
-        else:
-            self.subjects[self.choice].decrease(self.speed, time_)
+        activities = 0
+        for subject in self.subjects:
+            if subject.inuse:
+                activities += 1
+        if activities < self.max_activities:
+            activate_speed = self.speed * time_
+            if self.session:
+                activate_speed /= 2
+            random.choice(self.subjects).activate(activate_speed)
+            self.speed += self.delta * time_
 
-            if self.subjects[self.choice].percent <= 0:
-                self.gameover = True
+        for subject in self.subjects:
+            subject.update(self.speed * time_)
+
+        if not self.session:
+            self.progress += self.progress_speed * time_
+            if self.progress >= 100:
+                self.progress = 100
+                self.session = True
+                self.session_start = time.monotonic()
+                self.session_duration = random.uniform(0, 1) * 100
+                self.max_activities = 3
+        else:
+            current_time = time.monotonic()
+            if current_time > self.session_start + self.session_duration:
+                self.session = False
+                self.max_activities = 1
+                self.progress = 0
 
         self.pos = pygame.mouse.get_pos()
 
@@ -122,6 +170,12 @@ class Game:
         score_text = self.fonts['score'].render('ДОБОРЫ: ' + str(self.score), True, (0, 0, 0))
         self.canvas.blit(score_text, (600, 0))
 
+        pygame.draw.line(self.canvas, (0, 255, 0), (0, self.screen_size[1]-20),
+                         (self.progress * self.screen_size[0] / 100, self.screen_size[1]-20), 30)
+
+        if self.session:
+            session_text = self.fonts['session'].render('СЕССИЯ!!!', True, (255, 0, 0))
+            self.canvas.blit(session_text, (self.screen_size[0] / 2 - 50, self.screen_size[1]-40))
 
         if self.gameover:
             self.canvas.blit(self.final_screen, (0, 0))
@@ -134,5 +188,5 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game((800, 600), "ЫЫЫЫЫ")
+    game = Game((960, 720), "ЫЫЫЫЫ")
     game.run()
